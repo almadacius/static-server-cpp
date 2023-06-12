5fcc14d3d80fcde02c1adc5e9c0a27f8b116ad76
Vinicius Almada - HOME <almadacius@outlook.com>
2023-06-12 00:18:36 -0300

    build: implement CppBuild2

--- etc/scripts/build.js
+++ etc/scripts/build.js
@@ -1,33 +1,81 @@
 #!/usr/bin/env node
 
-const { CppBuild } = require('@almadash/builder')
-const { logger: { simpleLogger: logger } } = require('@almadash/shelf')
+const { CppBuild, CppBuild2 } = require('@almadash/builder')
+const {
+	errorHandler: { NodeErrorHandler },
+} = require('@almadash/shelf')
 
 const { project } = require('./lib')
 
 // ================================================
 class Operation {
-	async run() {
-		try {
-			project.ensureDirs()
+	async buildProjectLegacy() {
+		const { srcDir, rootDir, buildDir, modulesDir } = project.paths
 
-			const { rootDir, srcDir, buildDir, modulesDir } = project.paths
-
-			const buildOp = new CppBuild({
-				baseDir: rootDir,
-				version: 20,
-			})
-			await buildOp.build({
+		const buildOp = new CppBuild({
+			baseDir: rootDir,
+			// baseDir: srcDir,
+			version: 11,
+			srcDir,
+			output: `${buildDir}/server`,
+			includes: [
 				srcDir,
-				output: `${buildDir}/server`,
-				includes: modulesDir,
-				// modulesDir: `${rootDir}/temp/local_modules`,
-			})
-		}
-		catch (err) {
-			logger.logError(err)
-			process.exit(1)
-		}
+				modulesDir,
+			],
+			// modulesDir: `${rootDir}/temp/local_modules`,
+		})
+		await buildOp.build()
+	}
+
+	// ================================================
+	async buildServer() {
+		const { rootDir, srcDir, buildDir, modulesDir } = project.paths
+
+		const buildOp = new CppBuild2({
+			version: 11,
+			entrypoint: `${srcDir}/main.cpp`,
+			output: `${buildDir}/server`,
+
+			baseDir: rootDir,
+			srcDir,
+			headerDir: srcDir,
+			includes: [
+				modulesDir,
+			],
+		})
+		await buildOp.build()
+	}
+
+	async buildTestStr() {
+		const { rootDir, srcDir, buildDir, modulesDir } = project.paths
+
+		const buildOp = new CppBuild2({
+			version: 11,
+			entrypoint: `${srcDir}/testStr.cpp`,
+			output: `${buildDir}/server`,
+
+			baseDir: rootDir,
+			srcDir,
+			headerDir: srcDir,
+			includes: [
+				modulesDir,
+			],
+		})
+		await buildOp.build()
+	}
+
+	// ================================================
+	async run() {
+		const errorHandler = new NodeErrorHandler({ rootDir: `${__dirname}/..` })
+		errorHandler.register()
+
+		project.ensureDirs()
+
+		// await this.buildProjectLegacy()
+		// await this.buildTestStr()
+		await this.buildServer()
+
+		process.exit(0)
 	}
 }
 
